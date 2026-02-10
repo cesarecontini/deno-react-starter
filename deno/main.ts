bc1c72d61a8556ccf625fe55f450dc05e49c9625
Cesare Giani Contini <cesarecontini@gmail.com>
2026-02-10 12:56:15 +0100

    amended main.ts

--- deno/main.ts
+++ deno/main.ts
@@ -2,13 +2,9 @@ import { Context, Hono } from "hono";
 import countriesRoute from "./routes/countries.ts";
 
 const app = new Hono();
-
 app.get("/health", (c: Context) => {
   return c.json({ message: "ok" });
 });
-
 app.route("/countries", countriesRoute);
-
 app.route("/api/v1", app);
-
 Deno.serve(app.fetch);
