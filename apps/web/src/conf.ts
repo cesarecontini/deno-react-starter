37e15549ee03b401240d166c3e8aeb8a8b043ac3
Cesare Giani Contini <cesarecontini@gmail.com>
2026-02-11 11:52:50 +0100

    amended conf file for api url detection

--- apps/web/src/conf.ts
+++ apps/web/src/conf.ts
@@ -1,3 +1,26 @@
+const getApiUrlByDomain = () => {
+  const protocol = window.location.protocol;
+  const origin = window.location.origin;
+
+  if (protocol === "http:") {
+    return import.meta.env.VITE_API_URL;
+  }
+
+  if (origin.includes("--test")) {
+    return import.meta.env.VITE_API_URL_TEST;
+  }
+
+  if (origin.includes("--main")) {
+    return import.meta.env.VITE_API_URL_PROD;
+  }
+
+  if (origin === import.meta.env.VITE_API_URL_PROD) {
+    return import.meta.env.VITE_API_URL_PROD;
+  }
+
+  throw new Error("Invalid API URL");
+};
+
 export const conf = {
-    API_URL: import.meta.env.VITE_API_URL,
+  API_URL: getApiUrlByDomain(),
 };
